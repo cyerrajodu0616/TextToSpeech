e95f344d0ef21ac4ca3fa9c15bd20b8b0e9f1f65
ycrag <yc.raghava@gmail.com>
2023-12-31 13:00:49 -0800

    updated male and female voices

--- code/SingleFileTTS.py
+++ code/SingleFileTTS.py
@@ -1,22 +1,40 @@
 import pyttsx3
 import datetime
 
-Text_list = [
-              '''Adorable Toddler's Hilarious Concern During FaceTime '''
-              ,'''My toddler dropped my phone while she was FaceTiming my mom, gasped, picket it back up, and asked   '''
-              ,'''"grandma, are you ok???" '''
-              , ''' It was cutest, dumbest thing I have ever seen.'''
+Text_list_Female = [
+               '''Leaving Walmart, Halfway home. Why sweetiee?'''
+              , ''' OH DARN!! Be there in a Bit..'''
+             ]
+
+Text_list_Male = [
+              '''Mom, Where are You???'''
+              , ''' You brought me to walmart with you!!'''
              ]
 
 i = 0
-for text in Text_list:
-    engine = pyttsx3.init();
 
+engine = pyttsx3.init()
+engine.setProperty("speed",50)
+voices = engine.getProperty('voices')
 
-    engine.setProperty("speed",50)
+for v in voices:
+    print(v)
+
+for text in Text_list_Male:
+    engine.setProperty("voice", voices[0].id)
     text_input = text
 #engine.say(text_input);
-    engine.save_to_file(text_input,f"C:\\Users\\ycrag\\work\\gitHubRepo\\TextToSpeech\\content\\cutest{i}.mp3")
+    engine.save_to_file(text_input,f"C:\\Users\\ycrag\\work\\gitHubRepo\\TextToSpeech\\content\\shopping_Male_{i}.mp3")
     i+=1
 
-engine.runAndWait() ;
+
+for text in Text_list_Female:
+    voices = engine.getProperty('voices')
+    print(voices)
+    engine.setProperty("voice", voices[1].id)
+    text_input = text
+#engine.say(text_input);
+    engine.save_to_file(text_input,f"C:\\Users\\ycrag\\work\\gitHubRepo\\TextToSpeech\\content\\shopping_Female_{i}.mp3")
+    i+=1
+
+engine.runAndWait()
--- code/pytts.py
+++ code/pytts.py
@@ -1,7 +1,9 @@
 import pyttsx3
 engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+print(voices)
+engine.setProperty("voice",voices[1].id)
 engine.setProperty("speed",150)
 text_input = 'If you want to see the goodness of someone, then take advice from him.'
 engine.say(text_input)
-engine.save_to_file(text_input,"text_input.mp3")
 engine.runAndWait()
